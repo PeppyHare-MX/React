8f8c7b3da9c0539b48e4bfa2a1afae8b067067d5
seeand <framirez.work@gmail.com>
2020-07-06 13:15:48 -0500

    Setting up a dev environment with create-react-app

--- 2-IntroductionToComponents/npm-react-project/ultimate-components/src/index.js
+++ 2-IntroductionToComponents/npm-react-project/ultimate-components/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const Paragraph = () => <p>This is our app playground</p>
-
-ReactDOM.render(<Paragraph />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
