3600e708edb01d36585394d8fd99ba2203941d7c
framirez90 <framirez.work@gmail.com>
2020-07-07 10:23:49 -0500

    Understanding the key prop

--- 3-ADeeperDiveIntoComponents/deeper-dive-into-components/src/UltimateHolidayList.js
+++ 3-ADeeperDiveIntoComponents/deeper-dive-into-components/src/UltimateHolidayList.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 const list = [
-    'The beach',
-    'The mountains',
-    'Vibrant cities',
-    'Rough it',
-    'Ultimate survival'
+    { id: 1, name: 'The beach', },
+    { id: 3, name: 'The mountains', },
+    { id: 4, name: 'Vibrant cities', },
+    { id: 5, name: 'Rough it', },
+    { id: 6, name: 'Ultimate survival' }
 ];
 
 const UltimateHolidayList = () => (
     <section>
         <h1>UltimateHolidayList Destinations</h1>
-        <ul>
-            {list}
-        </ul>
+        <ul>{
+            list.map(item => (
+                <li key={item.id}>{item.name}</li>
+            ))
+        }</ul>
     </section>
 );
 
